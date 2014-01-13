a56e6e6be88ef4cfcb504909b8a1961334db57fb
stevec7 <stevec7@gmail.com>
2014-01-12 23:57:07 -0600

    added support for debugging, bad block output now in a better format

--- contrib/utils/validate_data.py
+++ contrib/utils/validate_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import datetime
+import logging
 import pickle
 from optparse import OptionParser
 from iointegrity.iotools import BlockMD5, FileMD5
@@ -7,30 +8,40 @@ from iointegrity.dbtools import IOIntegrityDB
 
 
 def main(options, args):
+    # setup logging
+    if options.verbose:
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+        loglvl = 'verbose'
+    else:
+        logging.basicConfig(format='%(message)s', level=logging.INFO)
+        loglvl = 'info'
+
     iodb = IOIntegrityDB(options.database)
     blk = BlockMD5()
-    fmd5 = FileMD5()
+    fmd5 = FileMD5(loglvl)
     date = datetime.datetime.now().isoformat()
 
     # get all file data from the database
     data = iodb.get_all_file_info()
 
-    print "Checking data integrity..."
+    logging.debug("DEBUG: Checking data integrity...")
     for d in data:
         name = d['name']
         md5sum = d['md5sum']
 
+        logging.debug("DEBUG: Validating {0}...".format(name))
         is_clean = fmd5.validate_md5(name, md5sum)
 
         if is_clean is not True:
-            print "File '{0}' doesn't match md5sum in database".format(name)
-            print "Doing block-level map check..."
+            logging.debug("DEBUG: File '{0}' doesn't match md5sum in database".format(name))
+            logging.debug("DEBUG: Doing block-level map check...")
             map = pickle.marshal.loads(d['chunks'])
             block_check = blk.validate_map(name, map)
             if block_check is not True:
-                print "Offset, Map_md5, Current_md5"
+                logging.debug("DEBUG: Name, Offset, Map_md5, Current_md5")
                 for b in block_check[1]:
-                    print b
+                    output = "{0},{1}".format(name, ','.join(str(e) for e in b))
+                    logging.info(output)
             
 
 if __name__ == '__main__':
@@ -38,5 +49,9 @@ if __name__ == '__main__':
     parser.add_option('-d','--db',
                         dest='database',
                         help='database file.')
+    parser.add_option('-v','--verbose',
+                        action='store_true',
+                        dest='verbose',
+                        help='print verbose statements')
     options, args = parser.parse_args()
     main(options, args)
