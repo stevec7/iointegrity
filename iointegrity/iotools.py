97de1bd45c6ec71bc04227d8545a5dce9b7e7395
stevec7 <stevec7@gmail.com>
2014-01-16 08:09:56 -0600

    added new class for working with a file tree

--- iointegrity/iotools.py
+++ iointegrity/iotools.py
@@ -90,6 +90,96 @@ class FileMD5(object):
         else:
             return True
 
+class FileTree(object):
+    def __init__(self, topdir, num_top_dirs=1, num_sub_dirs=0, 
+            third_level_dirs=0, files_per_dir=1, max_size=8192, 
+            aligned=True, stats=False, suffix=''):
+        '''Set the configuration'''
+        self.root = topdir
+        vfsstats = os.statvfs(self.root)
+        self.bufsize = vfsstats.f_bsize
+        self.num_top_dirs = num_top_dirs
+        self.num_sub_dirs = num_sub_dirs
+        self.max_size = max_size
+        self.files_per_dir = files_per_dir
+        self.aligned = aligned
+        self.stats = stats
+        self.suffix = suffix
+
+        return None
+
+    def _free_space(self, num_bytes):
+        '''Checks to see if there is enough space on the filesystem'''
+        vfsstats = os.path.dirname(os.path.abspath(self.root))
+
+        if num_bytes > (vfsstats.f_ffree * vfsstats.f_bfree):
+            return False
+        
+        return True
+
+    def _gen_all_dirs(self):
+        '''Create the directory hierarchy all at once'''
+        # make an array of directory paths
+        self.dirs = []
+
+        # if I was cool, I'd do some crazy algorithm or list comprehension
+        for topdir in range(0, self.num_top_dirs):
+            dirname = self.root + '/' + str(topdir)
+            self.dirs.append(dirname)
+            for subdir in range(0, self.num_sub_dirs):
+                dirname = self.root + '/' + str(topdir) + '/'+str(subdir)
+                self.dirs.append(dirname)
+                for subsubdir in range(0, self.third_level_dirs):
+                    dirname = self.root + '/' + str(topdir) + '/' + str(subdir)
+                    dirname = dirname + '/' + str(subsubdir)
+                    self.dirs.append(dirname)
+
+        return dirs
+
+    def _path_exists(self, filepath):
+        '''Checks to see if the path exists'''
+        if not os.path.isdir(os.path.dirname(filepath)):
+            return False
+
+        return True
+       
+
+    def write_file(self, filepath):       
+        '''Create a number of random files in a directory tree of varying size'''
+        # pick a random bytesize between 0 and max_size
+        if self.aligned:
+            num_bytes = random.randrange(self.bufsize, 
+                    stop=self.max_size, step=self.bufsize)
+        else:
+            num_bytes = random.randrange(1, self.max_size)
+
+        # check to see if enough space is available
+        if not self._free_space(num_bytes):
+            return "Not enough space to write data."
+        
+        # check to see if path exists
+        if not self._path_exists(filepath):
+            return "Directory does not exist."
+        
+        # figure out how many chunks we need to write
+        bytes_left = num_bytes
+
+        # add the suffix if specified
+        filepath = filepath+self.suffix
+
+        # write out the random data
+        with open(filepath, 'wb') as f:
+            while bytes_left > 0: 
+                if bytes_left < self.bufsize:
+                    f.write(os.urandom(bytes_left))
+                    bytes_left -= bufsize
+                else:
+                    f.write(os.urandom(bufsize))
+                    bytes_left -= bufsize
+        return
+
+# for when you don't want to use the FileTree class, 
+#   and simply want to create a random file
 def create_random_file(name, numbytes):
     '''writes out a file full of random data'''
     path = os.path.dirname(os.path.abspath(name))
